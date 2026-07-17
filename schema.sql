5736d9b31233ef30866f3e6383d520f2ad9e9580
sasichintada <sasichinthada248@gmail.com>
2026-07-17 21:51:46 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,79 @@
+-- Table 1: Departments
+
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50),
+    Location VARCHAR(50)
+);
+
+INSERT INTO Departments (DepartmentID, DepartmentName, Location) VALUES
+(1,'HR','New York'),
+(2,'Finance','Chicago'),
+(3,'IT','Dallas'),
+(4,'Marketing','Boston'),
+(5,'Sales','Seattle'),
+(6,'Operations','Atlanta');
+
+-- Table 2: Employees
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50),
+    DepartmentID INT,
+    Salary DECIMAL(10,2),
+    ManagerID INT,
+    JoiningDate DATE
+);
+
+INSERT INTO Employees
+(EmployeeID, EmployeeName, DepartmentID, Salary, ManagerID, JoiningDate)
+VALUES
+(101,'John',1,50000,NULL,'2020-01-15'),
+(102,'Emma',2,65000,101,'2021-03-10'),
+(103,'David',3,70000,101,'2019-07-22'),
+(104,'Sophia',3,72000,103,'2022-05-01'),
+(105,'Michael',5,55000,102,'2021-09-18'),
+(106,'Olivia',NULL,48000,102,'2023-01-12'),
+(107,'James',4,60000,103,'2022-10-20'),
+(108,'William',7,75000,101,'2020-08-11'),
+(109,'Ava',NULL,52000,NULL,'2024-02-15'),
+(110,'Isabella',5,68000,105,'2021-06-05');
+
+-- Table 3: Projects
+
+CREATE TABLE Projects (
+    ProjectID INT PRIMARY KEY,
+    ProjectName VARCHAR(50),
+    DepartmentID INT,
+    Budget DECIMAL(12,2)
+);
+
+INSERT INTO Projects
+(ProjectID, ProjectName, DepartmentID, Budget)
+VALUES
+(201,'Payroll System',1,150000),
+(202,'Audit System',2,200000),
+(203,'Website Redesign',4,100000),
+(204,'ERP Upgrade',3,500000),
+(205,'Sales Dashboard',5,175000),
+(206,'Automation',6,250000),
+(207,'AI Chatbot',8,300000);
+
+-- Table 4: EmployeeProjects
+
+CREATE TABLE EmployeeProjects (
+    EmployeeID INT,
+    ProjectID INT
+);
+
+INSERT INTO EmployeeProjects (EmployeeID, ProjectID) VALUES
+(101,201),
+(102,202),
+(103,204),
+(104,204),
+(105,205),
+(107,203),
+(110,205),
+(103,207),
+(108,207),
+(109,202);
